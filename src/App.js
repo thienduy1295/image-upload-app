f3c8a21ea0b0d2de590c08b2cb84fab94ae4780d
Thien Duy <thienduy1295@gmail.com>
2022-06-24 00:56:48 +0700

    App completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from "react";
+import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from "./app/config";
+import { useDropzone } from "react-dropzone";
+import { Image } from "cloudinary-react";
+import "./App.css";
+import CopyToClipboard from "react-copy-to-clipboard";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
+
+    acceptedFiles.forEach(async (acceptedFile) => {
+      const formData = new FormData();
+      formData.append("file", acceptedFile);
+      formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+      setLoading(true);
+
+      const response = await fetch(url, {
+        method: "POST",
+        body: formData,
+      });
+      const data = await response.json();
+      console.log(data);
+      setUploadedFiles((old) => [...old, data]);
+
+      setLoading(false);
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "image/*",
+    multiple: false,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload Image</h1>
+      <div
+        {...getRootProps()}
+        className={`dropzone ${isDragActive ? "active" : null}`}
+      >
+        <input {...getInputProps()} />
+        Drop Zone
+      </div>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <ul>
+          {uploadedFiles.map((file) => (
+            <li key={file.public_id}>
+              <Image
+                cloudName="duy-cloudinary"
+                publicId={file.public_id}
+                width="300"
+                crop="scale"
+              />
+              <CopyToClipboard text={file.secure_url}>
+                <button>Copy to clipboard</button>
+              </CopyToClipboard>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default App;
